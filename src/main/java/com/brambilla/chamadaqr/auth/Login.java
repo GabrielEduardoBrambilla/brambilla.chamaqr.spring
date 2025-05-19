74ca97faf236b7a0f3d48bf1097e07acf94e285f
Brambs <gabriel128.br@gmail.com>
2025-05-18 21:11:54 -0300

    sec

---
+++ src/main/java/com/brambilla/chamadaqr/auth/Login.java
@@ -0,0 +1,13 @@
+package com.brambilla.chamadaqr.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Login {
+
+	private String username;
+	private String password;
+	
+}
